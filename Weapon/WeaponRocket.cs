85e016e2a18018a39657ccb0dd58559b92f9902b
剑断了 <1655242948@qq.com>
2022-08-07 18:24:38 +0800

    Revert "武器系统"
    This reverts commit 347b747c784cd29e832faa773158f0db3659fb17.

---
+++ Weapon/WeaponRocket.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class WeaponRocket : IWeapon
+{
+    protected override void PlayBulletEffect(Vector3 targetPosition)
+    {
+        DoPlayBulletEffect(0.3f, targetPosition);
+    }
+
+    protected override void PlaySound()
+    {
+        DoPlaySound("RocketShot");
+    }
+
+    protected override void SetEffectDisplayTime()
+    {
+        mEffectDisplayTime = 0.4f;
+    }
+}
