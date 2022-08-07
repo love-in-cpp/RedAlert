f206ebf8732df1f2fca78365705eb78325978ed2
剑断了 <1655242948@qq.com>
2022-08-07 18:07:39 +0800

    push test

--- AchievementSystem/AchievementSystem.cs
+++ AchievementSystem/AchievementSystem.cs
@@ -4,16 +4,16 @@ public class AchievementSystem : IGameSystem
 {
     public override void Init()
     {
-        throw new NotImplementedException();
+        
     }
 
     public override void Update()
     {
-        throw new NotImplementedException();
+        
     }
 
     public override void Release()
     {
-        throw new NotImplementedException();
+        
     }
 }
