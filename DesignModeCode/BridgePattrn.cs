f35a0958df0bc4301875ac47e489e2eb62c3424e
剑断了 <1655242948@qq.com>
2022-08-08 19:51:01 +0800

    桥接模式理论原型

---
+++ DesignModeCode/BridgePattrn.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class BridgePattrn : MonoBehaviour
+{
+    private void Start()
+    {
+        ICharacter character = new SoldierCaptain();
+        character.Weapon = new WeaponGun();
+        // character.Attack(new Vector3(1,1,1));
+    }
+
+    private void Update()
+    {
+    }
+}
