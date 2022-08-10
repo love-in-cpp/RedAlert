22306e72979f072f659a7069b70d48f77242132c
剑断了 <1655242948@qq.com>
2022-08-10 17:26:14 +0800

    利用资源工厂创建武器绑定的GameObject对象，再调整其它参数。

---
+++ Factory/Weapon/WeaponFactory.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+
+public class WeaponFactory:IWeaponFactory
+{
+    public IWeapon CreatWeapon(WeaponType weaponType)
+    {
+        IWeapon weapon = null;
+        string assetName = "";
+        IAssetFactory factory = new ResourcesAssetFactory();
+        GameObject weaponGO;
+        switch (weaponType)
+        {
+            case WeaponType.Gun:
+                assetName = "WeaponGun";
+                weaponGO = factory.LoadWeapon(assetName);
+                weapon = new WeaponGun(20, 5, weaponGO);
+                break;
+            case WeaponType.Rifle:
+                assetName = "WeaponRifle";
+                weaponGO = factory.LoadWeapon(assetName);
+                weapon = new WeaponGun(30, 7, weaponGO);
+                break;
+            case WeaponType.Rocket:
+                assetName = "WeaponRocket";
+                weaponGO = factory.LoadWeapon(assetName);
+                weapon = new WeaponGun(40, 8, weaponGO);
+                break;
+        }
+
+        return weapon;
+
+
+
+
+    }
+}
