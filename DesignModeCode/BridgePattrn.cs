6640aba9f7b110028b271d5004435aeb7ed2e112
剑断了 <1655242948@qq.com>
2022-08-10 17:23:48 +0800

    注释无关代码

--- DesignModeCode/BridgePattrn.cs
+++ DesignModeCode/BridgePattrn.cs
@@ -5,7 +5,7 @@ public class BridgePattrn : MonoBehaviour
     private void Start()
     {
         ICharacter character = new SoldierCaptain();
-        character.Weapon = new WeaponGun();
+        // character.Weapon = new WeaponGun();
         // character.Attack(new Vector3(1,1,1));
     }
 
