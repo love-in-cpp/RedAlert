561dc032d786635e6582b20581e05cc5a6dd2416
剑断了 <1655242948@qq.com>
2022-08-10 16:35:35 +0800

    用接口实现了多方法还是单方法来区分抽象工厂模式和工厂方法模式

---
+++ Factory/Asset/LocalAssetFactory.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+namespace Factory.Asset
+{
+    public class LocalAssetFactory:IAssetFactory
+    {
+        public GameObject LoadSoldier(string name)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public GameObject LoadEnemy(string name)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public GameObject LoadWeapon(string name)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public GameObject LoadEffect(string name)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public GameObject LoadAudioClip(string name)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public GameObject LoadSprite(string name)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Factory/Asset/ResourcesAssetFactory.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class ResourcesAssetFactory:IAssetFactory
+{
+    public GameObject LoadSoldier(string name)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public GameObject LoadEnemy(string name)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public GameObject LoadWeapon(string name)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public GameObject LoadEffect(string name)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public GameObject LoadAudioClip(string name)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public GameObject LoadSprite(string name)
+    {
+        throw new System.NotImplementedException();
+    }
+}
