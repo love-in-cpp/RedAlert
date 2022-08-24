ac84cf208106e6029804f07e6cc5d3308161cf78
剑断了 <1655242948@qq.com>
2022-08-24 15:53:57 +0800

    开发抽象基类

---
+++ Factory/Character/Builder/ICharacterBuilder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+public abstract class ICharacterBuilder
+{
+    protected Type mT;
+    protected WeaponType mWeaponType;
+    protected Vector3 mSapawnPosition;
+    protected int mLv;
+
+    public ICharacterBuilder(Type t, WeaponType weaponType, Vector3 sapawnPosition, int lv)
+    {
+        mT = t;
+        mWeaponType = weaponType;
+        mSapawnPosition = sapawnPosition;
+        mLv = lv;
+    }
+    
+    
+    public abstract void AddCharacterAttr();
+    public abstract void AddGameObject();
+    public abstract void AddWeapon();
+}
---
+++ Factory/Character/Builder/SoldierBuilder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+public class SoldierBuilder:ICharacterBuilder
+{
+    public SoldierBuilder(Type t, WeaponType weaponType, Vector3 sapawnPosition, int lv) : base(t, weaponType, sapawnPosition, lv)
+    {
+    }
+
+    public override void AddCharacterAttr()
+    {
+    }
+
+    public override void AddGameObject()
+    {
+    }
+
+    public override void AddWeapon()
+    {
+    }
+}
