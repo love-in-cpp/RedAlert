d020f883eb36e904e043d5a95d938083bf9acd43
剑断了 <1655242948@qq.com>
2022-08-24 16:21:59 +0800

    完善敌人的建造者模式，以及在敌人工厂使用建造者模式

---
+++ Factory/Character/Builder/EnemyBuilder.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using UnityEngine;
+
+public class EnemyBuilder:ICharacterBuilder
+{
+    public EnemyBuilder(ICharacter character, Type t, WeaponType weaponType, Vector3 sapawnPosition, int lv) : base(
+        character, t, weaponType, sapawnPosition, lv)
+    {
+        
+    }
+
+    public override void AddCharacterAttr()
+    {
+        string name = "";
+        int maxHP = 0;
+        float moveSpeed = 0;
+        string iconSprite = "";
+        string mPrefabName = "";
+        
+        if (mT == typeof(EnemyElf))
+        {
+            name = "小精灵";
+            maxHP = 100;
+            moveSpeed = 3;
+            iconSprite = "ElfIcon";
+            mPrefabName = "Enemy1";
+        }
+        else if (mT == typeof(EnemyOrge))
+        {
+            name = "怪物";
+            maxHP = 120;
+            moveSpeed = 4;
+            iconSprite = "OgreIcon";
+            mPrefabName = "Enemy2";
+        }
+        else if (mT == typeof(EnemyOrge))
+        {
+            name = "巨魔";
+            maxHP = 200;
+            moveSpeed = 1;
+            iconSprite = "TrollIcon";
+            mPrefabName = "Enemy3";
+        }
+        else
+        {
+            Debug.LogError("类型" + mT.Name + "不属于ISoldier，无法创建敌人");
+        }
+        ICharacterAttr attr =
+            new EnemyAttr(new EnemyAttrStategy(), mLv, name, maxHP, moveSpeed, iconSprite, mPrefabName);
+        mCharacter.attr = attr;
+    }
+
+    public override void AddGameObject()
+    {
+        GameObject characterGO = FactoryManager.assetFactory.LoadEnemy(mPrefabName);
+        characterGO.transform.position = mSapawnPosition;// 生成的初始位置
+        mCharacter.gameObject = characterGO;
+    }
+
+    public override void AddWeapon()
+    {
+        mCharacter.weapon = FactoryManager.weaponFactory.CreatWeapon(mWeaponType);
+    }
+
+    public override ICharacter GetResult()
+    {
+        return mCharacter;
+    }
+}
--- Factory/Character/EnemyFactory.cs
+++ Factory/Character/EnemyFactory.cs
@@ -6,52 +6,8 @@ public class EnemyFactory:ICharacterFactory
     public ICharacter CreatCharacter<T>(WeaponType weaponType, Vector3 spawnPosition, int lv = 1) where T : ICharacter, new()
     {
         ICharacter character = new T();
-        string name = "";
-        int maxHP = 0;
-        float moveSpeed = 0;
-        string iconSprite = "";
-        string prefabName = "";
-
-        Type t = character.GetType();
-        if (t == typeof(EnemyElf))
-        {
-            name = "小精灵";
-            maxHP = 100;
-            moveSpeed = 3;
-            iconSprite = "ElfIcon";
-            prefabName = "Enemy1";
-        }
-        else if (t == typeof(EnemyOrge))
-        {
-            name = "怪物";
-            maxHP = 120;
-            moveSpeed = 4;
-            iconSprite = "OgreIcon";
-            prefabName = "Enemy2";
-        }
-        else if (t == typeof(EnemyOrge))
-        {
-            name = "巨魔";
-            maxHP = 200;
-            moveSpeed = 1;
-            iconSprite = "TrollIcon";
-            prefabName = "Enemy3";
-        }
-        else
-        {
-            Debug.LogError("类型" +t.Name+"不属于ISoldier，无法创建敌人");
-            return null;
-        }
-        ICharacterAttr attr =
-            new EnemyAttr(new EnemyAttrStategy(), lv, name, maxHP, moveSpeed, iconSprite, prefabName);
-        character.attr = attr;
-
-        GameObject characterGO = FactoryManager.assetFactory.LoadEnemy(prefabName);
-        characterGO.transform.position = spawnPosition;// 生成的初始位置
-        character.gameObject = characterGO;
-
-        character.weapon = FactoryManager.weaponFactory.CreatWeapon(weaponType);
-
-        return character;
+        
+        ICharacterBuilder builder = new SoldierBuilder(character, typeof(T), weaponType, spawnPosition, lv);
+        return CharacterBuilderDirector.Construct(builder);
     }
 }
