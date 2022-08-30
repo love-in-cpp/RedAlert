decd1c795db971face5175831f8d098f70f707c1
剑断了 <1655242948@qq.com>
2022-08-30 22:57:57 +0800

    组合模式理论原型

---
+++ DesignModeCode/CompositePattern.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+public class CompositePattern:MonoBehaviour
+{
+    private void Start()
+    {
+    }
+}
+public abstract class ICompoment
+{
+    public abstract void Operation();
+}
+
+public abstract class UnitControl: ICompoment//控件
+{
+}
+
+public class Button:UnitControl
+{
+    public override void Operation()
+    {
+    }
+}
+
+public class TextArea:UnitControl
+{
+    public override void Operation()
+    {
+    }
+}
+
+public abstract class Container:ICompoment
+{
+    protected List<Component> list;
+
+    public void AddComponent(ICompoment component)
+    {}
+
+    public void DeleteComponent(ICompoment component)
+    {}
+    public void GetCompoment(int index)
+    {}
+    
+}
+
+public class Widget : Container
+{
+    public override void Operation()
+    {
+    }
+}
+
+public class MiddlePanel : Container
+{
+    public override void Operation()
+    {
+    }
+}
+*/
