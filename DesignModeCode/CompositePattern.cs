9f47a9183fe94a193e9a40c944b93aff1a3f5465
剑断了 <1655242948@qq.com>
2022-08-31 23:25:07 +0800

    使用组合模式管理对象

--- DesignModeCode/CompositePattern.cs
+++ DesignModeCode/CompositePattern.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+// 作业
 
 /*
 public class CompositePattern:MonoBehaviour
@@ -60,3 +62,102 @@ public class MiddlePanel : Container
     }
 }
 */
+
+// 课程
+public class CompositePattern:MonoBehaviour
+{
+    private void Start()
+    {
+        DMComposite root = new DMComposite("Root");
+
+        DMLeaf leaf1 = new DMLeaf("GameObject");
+        DMLeaf leaf2 = new DMLeaf("GameObject (2)");
+        DMComposite gameObject1 = new DMComposite("GameObject (1)");
+        
+        root.AddChild(leaf1);
+        root.AddChild(gameObject1);
+        root.AddChild(leaf2);
+
+        DMLeaf child1 = new DMLeaf("GameObject");
+        DMLeaf child2 = new DMLeaf("GameObject (1)");
+        gameObject1.AddChild(child1);
+        gameObject1.AddChild(child2);
+        ReadComponent(root);
+    }
+
+    private void ReadComponent(DMComponent component)
+    {
+        Debug.Log(component.name);
+        List<DMComponent> children = component.children;
+        if (children == null || children.Count == 0) return;
+        foreach (var child in children)
+        {
+            ReadComponent(child);
+        }
+
+    }
+} 
+
+public abstract class DMComponent
+{
+    protected string mName; // name of GameObject
+    public string name => mName;
+    protected List<DMComponent> mChildren;
+    public List<DMComponent> children => mChildren;
+
+    public DMComponent(string name)
+    {
+        mChildren = new List<DMComponent>();
+        mName = name;
+    }
+
+    public abstract void AddChild(DMComponent c);
+
+    public abstract void RemoveChild(DMComponent c);
+
+    public abstract DMComponent GetChild(int index);
+
+}
+
+public class DMLeaf : DMComponent
+{
+    public DMLeaf(string name) : base(name)
+    {
+    }
+
+    public override void AddChild(DMComponent c)
+    {
+        
+    }
+
+    public override void RemoveChild(DMComponent c)
+    {
+    }
+
+    public override DMComponent GetChild(int index)
+    {
+        return null;
+    }
+}
+
+public class DMComposite : DMComponent
+{
+    public DMComposite(string name) : base(name)
+    {
+    }
+
+    public override void AddChild(DMComponent c)
+    {
+        mChildren.Add(c);
+    }
+
+    public override void RemoveChild(DMComponent c)
+    {
+        mChildren.Remove(c);
+    }
+
+    public override DMComponent GetChild(int index)
+    {
+        return mChildren[index];
+    }
+}
