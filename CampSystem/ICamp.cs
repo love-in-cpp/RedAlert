4220b3bdeffbab1c89e7251481c34c1b22d08cd7
剑断了 <1655242948@qq.com>
2022-09-01 21:51:24 +0800

    初始化兵营基类和士兵兵营

---
+++ CampSystem/ICamp.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public abstract class ICamp
+{
+    protected GameObject mGameObject;
+    protected string mName;
+    protected string mIconSprite;
+    protected SoldierType mSoldierType;
+    protected Vector3 mPosition; // 集合点
+
+    public ICamp(GameObject gameObject, string name, string icon, SoldierType soldierType, Vector3 position)
+    {
+        mGameObject = gameObject;
+        mName = name;
+        mIconSprite = icon;
+        mSoldierType = soldierType;
+        mPosition = position;
+    }
+
+}
---
+++ CampSystem/SoldierCamp.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class SoldierCamp:ICamp
+{
+    private int mLv = 1;
+    private const int MAX_LV = 4;
+    private WeaponType mWeaponType = WeaponType.Gun; // 利用枚举类型的自增完成升级
+
+    public SoldierCamp(GameObject gameObject, string name, string icon, SoldierType soldierType, Vector3 position) 
+        : base(gameObject, name, icon, soldierType, position)
+    {
+    }
+    
+}
