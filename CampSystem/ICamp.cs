a463bacad12700df864d20a1251cb203e4857821
剑断了 <1655242948@qq.com>
2022-09-03 16:07:05 +0800

    添加计时器控制训练命令的执行，并对取消命令进行补充

--- CampSystem/ICamp.cs
+++ CampSystem/ICamp.cs
@@ -12,6 +12,7 @@ public abstract class ICamp
     protected float mTrainTime;
 
     protected List<ITrainCommand> mCommands;
+    private float mTrainTimer = 0;
     public ICamp(GameObject gameObject, string name, string icon, SoldierType soldierType, Vector3 position, float trainTime)
     {
         mGameObject = gameObject;
@@ -20,13 +21,26 @@ public abstract class ICamp
         mSoldierType = soldierType;
         mPosition = position;
         mTrainTime = trainTime;
+        mTrainTimer = mTrainTime;
 
         mCommands = new List<ITrainCommand>();
     }
 
     public virtual void Update()
     {
-        
+        UpdateCommand();
+    }
+
+    private void UpdateCommand()
+    {
+        if (mCommands.Count <= 0)return;
+        mTrainTimer -= Time.deltaTime;
+        if (mTrainTimer <= 0)
+        {
+            mCommands[0].Execute();
+            mCommands.RemoveAt(0);
+            mTrainTimer = mTrainTime;
+        }
     }
 
     public string name => mName;
@@ -41,6 +55,11 @@ public abstract class ICamp
         if (mCommands.Count > 0)
         {
             mCommands.RemoveAt(mCommands.Count-1);
+            // Timer重置！!
+            if (mCommands.Count == 0)
+            {
+                mTrainTimer = mTrainTime;
+            }
         }
     }
 }
