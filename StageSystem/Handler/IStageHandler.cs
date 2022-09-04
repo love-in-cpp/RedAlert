437d43200588d8c92699b905f44aede38922e27a
剑断了 <1655242948@qq.com>
2022-09-04 15:41:42 +0800

    使用责任链模式开发关卡系统，初步。

---
+++ StageSystem/Handler/IStageHandler.cs
@@ -0,0 +1,26 @@
+﻿
+public abstract class IStageHandler
+{
+    protected int mLv; // 当前关卡
+    protected IStageHandler mNextHandler;
+
+    public IStageHandler SetNextHandler(IStageHandler handler)
+    {
+        mNextHandler = handler;
+        return mNextHandler;
+    }
+    
+    public void Handle(int level)
+    {
+        if (level == mLv)
+        {
+            UpdateStage();      
+        }
+        else
+        {
+            mNextHandler.Handle(level);
+        }
+        
+    }
+    protected virtual void UpdateStage() {}
+}
---
+++ StageSystem/Handler/NormalStageHandler.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class NormalStageHandler:IStageHandler
+{
+    private EnemyType mEnemyType;
+    private WeaponType mWeaponType;
+    private int mCount;
+    private Vector3 mPosition;
+
+    protected override void UpdateStage()
+    {
+        base.UpdateStage();
+    }
+}
