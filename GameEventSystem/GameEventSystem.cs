df175368d4c219468c3e8551cba5c3257beb545f
剑断了 <1655242948@qq.com>
2022-09-06 15:08:39 +0800

    观察者模式：相比初始化时加载，注册时加载可以避免各个系统之间Init先后顺序导致的问题

--- GameEventSystem/GameEventSystem.cs
+++ GameEventSystem/GameEventSystem.cs
@@ -17,14 +17,11 @@ public class GameEventSystem : IGameSystem
     public override void Init()
     {
         base.Init();
-        InitGameEvents();
+        // InitGameEvents();
     }
 
     private void InitGameEvents()
     {
-        mGameEvents.Add(GameEventType.EnemyKilled, new EnemyKilledSubject());
-        mGameEvents.Add(GameEventType.SoldierKilled, new SoldierKilledSubject());
-        mGameEvents.Add(GameEventType.NewStage, new NewStageSubject());
     }
 
     public void RegisterObserver(GameEventType eventType, IGameEventObserver observer)
@@ -47,8 +44,21 @@ public class GameEventSystem : IGameSystem
     {
         if (!mGameEvents.ContainsKey(eventType))
         {
-            Debug.LogError("没有对应事件类型" + eventType + "的主题类");
-            return null;
+            switch (eventType)
+            {
+                case GameEventType.EnemyKilled:
+                    mGameEvents.Add(GameEventType.EnemyKilled, new EnemyKilledSubject());
+                    break;
+                case GameEventType.SoldierKilled:
+                    mGameEvents.Add(GameEventType.SoldierKilled, new SoldierKilledSubject());
+                    break;
+                case GameEventType.NewStage:
+                    mGameEvents.Add(GameEventType.NewStage, new NewStageSubject());
+                    break;
+                default:
+                    Debug.LogError("没有对应事件类型" + eventType + "的主题类");
+                    return null;
+            }
         }
         return mGameEvents[eventType];
     }
