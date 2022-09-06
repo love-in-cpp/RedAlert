55b1dcb6c0398728d1218c578c1050d733793a8c
剑断了 <1655242948@qq.com>
2022-09-06 12:35:05 +0800

    创建subject和observer基类

---
+++ GameEventSystem/Subject/IGameEventSubject.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+public abstract class IGameEventSubject
+{
+    private List<IGameEventObserver> mObservers = new List<IGameEventObserver>();
+
+    public void RegisterObserver(IGameEventObserver ob)
+    {
+        mObservers.Add(ob);
+    }
+
+    public void RemoveObserver(IGameEventObserver ob)
+    {
+        mObservers.Remove(ob);
+    }
+
+    public void Notify()
+    {
+        foreach (var ob in mObservers)
+        {
+            ob.Update();
+        }
+    }
+    
+}
