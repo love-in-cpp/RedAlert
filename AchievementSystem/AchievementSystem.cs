da90e13fad8892cfe36632198ddf363f424a5998
剑断了 <1655242948@qq.com>
2022-09-06 16:07:40 +0800

    成就系统：统计累计杀敌数量

--- AchievementSystem/AchievementSystem.cs
+++ AchievementSystem/AchievementSystem.cs
@@ -2,10 +2,33 @@
 
 public class AchievementSystem : IGameSystem
 {
+    // 本成就系统只保存一些最大数据
+    private int mEnemyKilledCount = 0;
+    private int mSoldierKilledCount = 0;
+    private int mMaxStageLv = 1;
+
+    public void AddEnemyKilledCount(int number = 1)
+    {
+        mEnemyKilledCount += number;
+    }
+
+    public void AddSoldierKilledCount(int number = 1)
+    {
+        mSoldierKilledCount += number;
+    }
+
+    public void SetMaxStageLv(int stageLv)
+    {
+        if (stageLv > mMaxStageLv)
+        {
+            mMaxStageLv = stageLv;
+        }
+    }
+    
     public override void Init()
     {
         base.Init();
-        
+        mGameFacade.RegisterObserver(GameEventType.EnemyKilled, new EnemyKilledObserverAchievement(this));
     }
 
     public override void Update()
---
+++ GameEventSystem/Observer/EnemyKilledObserver/EnemyKilledObserverAchievement.cs
@@ -0,0 +1,20 @@
+﻿public class EnemyKilledObserverAchievement:IGameEventObserver
+{
+    // private EnemyKilledSubject mSubject;
+    private AchievementSystem mAchSystem;
+
+    public EnemyKilledObserverAchievement(AchievementSystem system)
+    {
+        mAchSystem = system;
+    }
+    public override void Update()
+    {
+        mAchSystem.AddEnemyKilledCount();
+    }
+
+    public override void SetSubject(IGameEventSubject sub)
+    {
+        // mSubject = sub as EnemyKilledSubject;
+    }
+    
+}
