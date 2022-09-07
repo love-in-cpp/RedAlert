c7892c631a948970fdd71c1c8621bdf019b9ddfb
剑断了 <1655242948@qq.com>
2022-09-07 13:36:56 +0800

    备忘录模式：备忘录，使用PlayerPrefs类将数据保存到硬盘 无负责人是因为并不区分版本数据，无需新增 字典 的开销

---
+++ AchievementSystem/AchievenmentMemento.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class AchievenmentMemento
+{
+    // 本成就系统只保存一些最大数据
+    public int enemyKilledCount { get; set; }
+    public int soldierKilledCount{ get; set; }
+    public int maxStageLv{ get; set; }
+
+    public void SaveData()
+    {
+        PlayerPrefs.SetInt("EnemyKilledCount", enemyKilledCount);
+        PlayerPrefs.SetInt("SoldierKilledCount", soldierKilledCount);
+        PlayerPrefs.SetInt("MaxStageLv", maxStageLv);
+    }
+
+    public void LoadData()
+    {
+        enemyKilledCount=PlayerPrefs.GetInt("EnemyKilledCount" );
+        soldierKilledCount=PlayerPrefs.GetInt("SoldierKilledCount" );
+        maxStageLv=PlayerPrefs.GetInt("MaxStageLv" );
+    }
+    
+}
